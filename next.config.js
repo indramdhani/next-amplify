167ca330474ddf052d3c5c7117a5d6581b676b9b
indra ramdhani <indramdhani@gmail.com>
2023-09-07 15:49:40 +0700

    revert back the app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/new-path',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/new-path',
+        basePath: false,
+        permanent: false
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
