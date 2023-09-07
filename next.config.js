224ce46425401609805887763d0f8d6b14804185
indra ramdhani <indramdhani@gmail.com>
2023-09-07 10:24:25 +0700

    setuup redirect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/new-path'
+  basePath: '/new-path',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/new-path',
+        basePath: false,
+        permanent: false
+      }
+    ];
+  };
 };
 
 module.exports = nextConfig;
